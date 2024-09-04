64fd2a50df18f97d4cbb367ea5d7c62e44d7ed15
Muhammed Salman <muhammedsalman181818@gmail.com>
2024-09-04 21:27:39 +0530

    Resolved merge conflicts

--- First_Project/src/App.vue
+++ First_Project/src/App.vue
@@ -1,5 +1,24 @@
-
+<script>
+export default{
+    data(){
+        return {
+            name:"John Doe",
+            status:true,
+            tasks:['one','two','three'],
+            link:"https://www.google.com/"
+        }
+    }
+}
+</script>
 
 <template>
- <h1>nasdcdsfhnd</h1>
+ <h1>{{ name }}</h1>
+ <p v-if="status">User is active</p>
+ <p v-else="status">User is not active</p>
+ <a v-bind:href="link" href="">sdsdhcudsh</a>
+ <h1>Tasks</h1>
+ <ul>
+    <li v-for="task in tasks">{{ task }}</li>
+ </ul>
+
 </template>
